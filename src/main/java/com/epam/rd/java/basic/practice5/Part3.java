5d0582e9641e0584723bd54091207d31ec7b0542
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-07-25 19:57:50 +0300

    Part1

--- src/main/java/com/epam/rd/java/basic/practice5/Part3.java
+++ src/main/java/com/epam/rd/java/basic/practice5/Part3.java
@@ -6,6 +6,10 @@ public class Part3 {
 
     private int counter2;
 
+    public Part3(int numberOfThreads, int numberOfIterations) {
+
+    }
+
     public static void main(final String[] args) {
 
     }
