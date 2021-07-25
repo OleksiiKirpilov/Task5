58a307e9ab4f9adba8ed4819344778176e37084d
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-07-25 19:54:24 +0300

    Part1

--- src/main/java/com/epam/rd/java/basic/practice5/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice5/Part1.java
@@ -1,9 +1,40 @@
 package com.epam.rd.java.basic.practice5;
 
-public class Part1 {
+import java.util.logging.Logger;
+
+public class Part1 extends Thread {
+
+    public static void worker() {
+        for (int i = 0; i < 4; i++) {
+            System.out.println(currentThread().getName());
+            try {
+                sleep(500);
+            } catch (InterruptedException e) {
+                Logger.getGlobal().severe(e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        worker();
+    }
 
     public static void main(String[] args) {
-
+        Thread t1 = new Part1();
+        Thread t2 = new Thread(Part1::worker);
+        t1.start();
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            Logger.getGlobal().severe(e.getMessage());
+        }
+        t2.start();
+        try {
+            t2.join();
+        } catch (InterruptedException e) {
+            Logger.getGlobal().severe(e.getMessage());
+        }
     }
 
 }
