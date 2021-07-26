db13c58d37cad547692a7c12c56d9e859777ea0a
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-07-26 11:20:19 +0300

    Part2

--- src/main/java/com/epam/rd/java/basic/practice5/Part2.java
+++ src/main/java/com/epam/rd/java/basic/practice5/Part2.java
@@ -1,9 +1,81 @@
 package com.epam.rd.java.basic.practice5;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Logger;
+
 public class Part2 {
+
+    private static final InputStream STD_IN = System.in;
+    private static final InputStream MOCK_STREAM = new MockStream();
+    private static int INPUT_BYTE = -1;
+
+    private static final class MockStream extends InputStream {
+
+        boolean done = false;
+
+        @Override
+        public int read() throws IOException {
+            if (done) {
+                return -1;
+            } else {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    Logger.getGlobal().severe(e.getMessage());
+                    Thread.currentThread().interrupt();
+                }
+                done = true;
+                return '\n';
+            }
+    //            int out = INPUT_BYTE;
+    //            INPUT_BYTE = -1;
+    //            return out;
+        }
+    }
+
+//    private static class Mocker implements Runnable {
+//
+//        @Override
+//        public void run() {
+//            while (true) {
+//                try {
+//                    Thread.sleep(2000);
+//                } catch (InterruptedException e) {
+//                    Logger.getGlobal().severe(e.getMessage());
+//                    Thread.currentThread().interrupt();
+//                    break;
+//                }
+//                INPUT_BYTE = (int) '\n';
+//            }
+//        }
+//    }
 
     public static void main(final String[] args) {
 
+        System.setIn(MOCK_STREAM);
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                Spam.main(null);
+            }
+        };
+//        Thread mt = new Thread(new Mocker());
+        t.start();
+//        mt.start();
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            Logger.getGlobal().severe(e.getMessage());
+            t.interrupt();
+        }
+//        try {
+//            mt.join();
+//        } catch (InterruptedException e) {
+//            Logger.getGlobal().severe(e.getMessage());
+//            mt.interrupt();
+//        }
+        System.setIn(STD_IN);
     }
 
 }
--- src/main/java/com/epam/rd/java/basic/practice5/Spam.java
+++ src/main/java/com/epam/rd/java/basic/practice5/Spam.java
@@ -1,27 +1,77 @@
 package com.epam.rd.java.basic.practice5;
 
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class Spam {
 
     private Thread[] threads;
 
     public Spam(final String[] messages, final int[] delays) {
-
+        int num = messages.length;
+        threads = new Worker[num];
+        for (int i = 0; i < num; i++) {
+            threads[i] = new Worker(messages[i], delays[i]);
+        }
     }
 
     public static void main(final String[] args) {
-
+        Spam spam = new Spam(new String[]{"@@@", "bbbbbbb"}, new int[]{500, 333});
+        spam.start();
+        Scanner in = new Scanner(System.in);
+        while (!in.hasNextLine() || !in.nextLine().isEmpty()) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                Logger.getGlobal().severe(e.getMessage());
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+        spam.stop();
     }
 
     public void start() {
-
+        for (Thread t : threads) {
+            t.start();
+        }
     }
 
     public void stop() {
-
+        for (Thread t : threads) {
+            t.interrupt();
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                Logger.getGlobal().severe(e.getMessage());
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 
     private static class Worker extends Thread {
 
+        private final String message;
+        private final int delay;
+
+        private Worker(String message, int delay) {
+            this.message = message;
+            this.delay = delay;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    sleep(delay);
+                } catch (InterruptedException e) {
+                    //Logger.getGlobal().severe(e.getMessage());
+                    currentThread().interrupt();
+                    break;
+                }
+                System.out.println(message);
+            }
+        }
     }
 
 }
